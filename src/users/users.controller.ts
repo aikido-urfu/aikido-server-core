cc0c071d0a893f5afed686aba0d993a2159a0de2
v1ab1 <sharkandmouse@gmail.com>
2023-11-19 00:06:49 +0500

    api specs added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Get,
-  Post,
   Body,
   Patch,
   Param,
@@ -9,11 +8,39 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { UserId } from 'src/decorators/user-id.decorator';
+
+const userProfile = {
+  schema: {
+    type: 'object',
+    properties: {
+      id: {
+        type: 'number',
+      },
+      login: {
+        type: 'string',
+      },
+      password: {
+        type: 'string',
+      },
+      email: {
+        type: 'string',
+      },
+      phone: {
+        type: 'string',
+      },
+    },
+  },
+};
 
 @Controller('users')
 @ApiTags('users')
@@ -21,48 +48,32 @@ import { UserId } from 'src/decorators/user-id.decorator';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        login: {
-          type: 'string',
-        },
-        password: {
-          type: 'string',
-        },
-        email: {
-          type: 'string',
-        },
-        phone: {
-          type: 'string',
-        },
-      },
-    },
-  })
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
   @Get(':id')
+  @ApiConsumes('multipart/form-data')
+  @ApiResponse(userProfile)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Get('/me')
   @UseGuards(JwtAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        token: { type: 'string' },
+      },
+    },
+  })
+  @ApiResponse(userProfile)
   getMe(@UserId() id: number) {
     return this.usersService.findById(id);
   }
 
   @Patch(':id')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody(userProfile)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
--- src/votes/votes.controller.ts
+++ src/votes/votes.controller.ts
@@ -10,7 +10,7 @@ import {
 import { VotesService } from './votes.service';
 import { CreateVoteDto } from './dto/create-vote.dto';
 import { UpdateVoteDto } from './dto/update-vote.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('votes')
 @ApiTags('votes')
@@ -18,26 +18,167 @@ export class VotesController {
   constructor(private readonly votesService: VotesService) {}
 
   @Post()
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        title: {
+          type: 'string',
+        },
+        fields: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              field: {
+                type: 'string',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'number',
+        },
+      },
+    },
+  })
   create(@Body() createVoteDto: CreateVoteDto) {
     return this.votesService.create(createVoteDto);
   }
 
   @Get()
+  @ApiConsumes('multipart/form-data')
+  @ApiResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        votes: {
+          type: 'array',
+          properties: {
+            title: {
+              type: 'string',
+            },
+            fields: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  field: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
   findAll() {
     return this.votesService.findAll();
   }
 
   @Get(':id')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'number',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        title: {
+          type: 'string',
+        },
+        fields: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              field: {
+                type: 'string',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
   findOne(@Param('id') id: string) {
     return this.votesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'number',
+        },
+        title: {
+          type: 'string',
+        },
+        fields: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              field: {
+                type: 'string',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
   update(@Param('id') id: string, @Body() updateVoteDto: UpdateVoteDto) {
     return this.votesService.update(+id, updateVoteDto);
   }
 
   @Delete(':id')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'number',
+        },
+        title: {
+          type: 'string',
+        },
+        fields: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              field: {
+                type: 'string',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
   remove(@Param('id') id: string) {
     return this.votesService.remove(+id);
   }
