6a4e0d67b8a38c48d6e950d9c0012689c11f9c1a
v1ab1 <sharkandmouse@gmail.com>
2023-11-27 00:26:37 +0300

    hiding swagger in prod

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,15 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    });
+  }
 
   await app.listen(3000);
 }
