3cf89aae543a05f6ba8a118558b95d5562932e3b
v1ab1 <sharkandmouse@gmail.com>
2023-12-15 17:03:16 +0300

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ credentials: true, origin: true });
+
   const config = new DocumentBuilder()
     .setTitle('Aikido')
     .setVersion('1.0')
