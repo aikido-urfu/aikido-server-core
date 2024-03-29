0dded89faf78fd2dbbef67d997eee5bd6c015f67
PPnotGG <ave.ppgg@gmail.com>
2024-03-29 20:38:06 +0500

    changed urls

--- API_URL.ts
+++ API_URL.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://venchass.ru:3005/';
-export const Frontend_URL = 'http://venchass.ru:3004/';
-export const Files_URL = 'http://venchass.ru:3006/';
+export const API_URL = 'http://localhost:3005/';
+export const Frontend_URL = 'http://localhost:3004/';
+export const Files_URL = 'http://localhost:3006/';
